ce3ab04738087cda29ae8b03c2df325d85a9795a
dklounge <daviddarden11@gmail.com>
2013-05-04 19:51:07 -0700

    updated ideas route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Thinkado::Application.routes.draw do
+  root 'ideas#index'
+
+  resources :ideas  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
