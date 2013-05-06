f789b591d5ff0589134305d2017a81e39d8443e5
dklounge <daviddarden11@gmail.com>
2013-05-06 00:09:16 -0700

    added rspec and annotate gems to gemfile

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -12,26 +12,32 @@ class IdeasController < ApplicationController
   end
 
   def create
-    @idea = Idea.new(idea_params)
-        
-    respond_to do |f|
-      if @idea.save
-        format.html { redirect_to @idea, notice: "Thanks for your great idea!" }
-        format.json { render json: @idea, status: :created, location: @idea }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @idea.errors, status: :unprocessable_entity }
-      end
+  #  @idea = Idea.new(idea_params)
+    @idea = Idea.new(params[:idea])
+    if @idea.save
+      redirect_to @idea
+    else
+      render action :new
     end
   end
+        
+  #   respond_to do |f|
+  #     if @idea.save
+  #       format.html { redirect_to @idea, notice: "Thanks for your great idea!" }
+  #       format.json { render json: @idea, status: :created, location: @idea }
+  #     else
+  #       format.html { render action: "new" }
+  #       format.json { render json: @idea.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-    private
-      # In rails 4, used to whitelist the permissible parameters.
-      # Example: params.require(:person).permit(:name, :age)
-      def idea_params
-        params.require(:idea).permit(:title, :body)
-      end
-  
+  #   private
+  #     # In rails 4, used to whitelist the permissible parameters.
+  #     # Example: params.require(:person).permit(:name, :age)
+  #     def idea_params
+  #       params.require(:idea).permit(:title, :body)
+  #     end
   
     
 end
