a2f895aa56dfa2b17c10e11d49499bdd087cfd80
dklounge <daviddarden11@gmail.com>
2013-06-18 18:04:13 -0700

    added comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
+  attr_accessible :author_name, :body
   belongs_to :idea
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,8 +2,8 @@ class Idea < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
   attr_accessible :title, :body
 
-  
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true, uniqueness: true
 
+  has_many :comments
 end
