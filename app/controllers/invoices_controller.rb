7196a03492d675f8f6f02925ba86184e63570c04
dklounge <daviddarden11@gmail.com>
2013-07-31 22:26:53 -0700

    change

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -3,7 +3,7 @@ class InvoicesController < ApplicationController
   def index
   #	@clients = xero_client.Contact.all
   	@methods = xero_client.methods.sort
-  	@invoices = xero_client.Invoice
+  	@invoices = xero_client.Invoice.all
   end
 
 	def show
